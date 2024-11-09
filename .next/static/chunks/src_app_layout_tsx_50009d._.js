4a8f99404791592f2fc5a774dff5882380ff33de
Arvish Pandey <arvishpandey@icloud.com>
2024-11-08 19:59:46 -0500

    backtracking commit
    attempt to infinite redirect

--- .next/static/chunks/src_app_layout_tsx_50009d._.js
+++ .next/static/chunks/src_app_layout_tsx_50009d._.js
@@ -6,7 +6,7 @@
   "path": "static/chunks/src_app_layout_tsx_50009d._.js",
   "chunks": [
     "static/chunks/src_app_globals_b80590.css",
-    "static/chunks/node_modules__pnpm_8d4b46._.js",
+    "static/chunks/node_modules__pnpm_e90738._.js",
     "static/chunks/src_app_8447dd._.js"
   ],
   "source": "dynamic"
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://wealthy-foxhound-58.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
